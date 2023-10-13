20a1f805b09af0953d0448a43583071e862212c8
Ricardo Torres <giron.torres1@gmail.com>
2023-10-13 11:21:49 -0600

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import "../src/hojas-de-estilo/Tarea.css";
+import TareaFormulario from './componentes/TareaFormulario';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="aplicacion-tareas">
+      <div className='tareas-lista-principal'>
+        <h1>Mis tareas</h1>
+        <TareaFormulario>
+          
+        </TareaFormulario>
+      </div>
     </div>
   );
 }
