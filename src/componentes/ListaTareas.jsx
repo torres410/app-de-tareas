5b7aa77d9fe85338bd6629e6e9e2709753133ba5
Ricardo Torres <giron.torres1@gmail.com>
2023-10-15 15:56:31 -0600

    Nuevas funciones constantes y mejoramiento del return

--- src/componentes/ListaTareas.jsx
+++ src/componentes/ListaTareas.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import TareaFormulario from "./TareaFormulario";
+import Tarea from "./Tarea";
+import "../hojas-de-estilo/ListaTareas.css";
 
 function ListaTareas() {
+
+  const [tareas, setTareas] = useState([]);
+
+  const agregarTarea = tarea => {
+    if (tarea.texto.trim()) {
+      tarea.texto = tarea.texto.trim();
+      const tareasActualizadas = [tarea, ...tareas];
+      setTareas(tareasActualizadas);
+    }
+  }
+
   return (
     <>
-      <TareaFormulario />
+      <TareaFormulario onSubmit={agregarTarea}/>
       <div className="tareas-lista-contenedor">
-        Lista de tareas
+        {
+          tareas.map(
+            (tarea) =>
+              <Tarea
+                key={tarea.id}
+                id={tarea.id}
+                texto={tarea.texto}
+                completada={tarea.completada}
+              />
+          )
+        }
       </div>
     </>
   );
