0f0aca0b626f785ae2192f49c8661c0e91a62fe6
Ricardo Torres <giron.torres1@gmail.com>
2023-10-16 00:56:08 -0600

    Se agrego la funcion para marcar tareas como completadas

--- src/componentes/ListaTareas.jsx
+++ src/componentes/ListaTareas.jsx
@@ -20,20 +20,29 @@ function ListaTareas() {
     setTareas(tareasActualizadas);
   }
 
+const completarTarea = id => {
+  const tareasActualizadas = tareas.map(tarea => {
+    if (tarea.id === id) {
+      tarea.completada = !tarea.completada;
+    }
+    return tarea;
+  });
+  setTareas(tareasActualizadas);
+}
+
   return (
     <>
       <TareaFormulario onSubmit={agregarTarea}/>
       <div className="tareas-lista-contenedor">
         {
-          tareas.map(
-            (tarea) =>
-              <Tarea
-                key={tarea.id}
-                id={tarea.id}
-                texto={tarea.texto}
-                completada={tarea.completada}
-                eliminarTarea={eliminarTarea}
-              />
+          tareas.map((tarea) =>
+            <Tarea
+              key={tarea.id}
+              id={tarea.id}
+              texto={tarea.texto}
+              completada={tarea.completada}
+              completarTarea={completarTarea}
+              eliminarTarea={eliminarTarea} />
           )
         }
       </div>
